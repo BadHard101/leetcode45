49046b76acfd888710875e848cb64e6d538eb716
BadHard <BadHard101@yandex.ru>
2024-01-21 09:07:37 +0300

    +sol

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,23 @@
 public class Main {
     public static int jump(int[] nums) {
         int ans = 0;
-        int pos = 0;
-        int reachable = nums[pos] + pos;
-        while (pos < nums.length - 1) {
-            int max = pos + nums[pos];
-            for (int i = pos + 1; i <= reachable; i++) {
-                if (reachable >= nums.length - 1) {
+        int cur = 0;
+        int reachable = nums[cur] + cur;
+        int max = nums[cur] + cur;
+
+        while (cur < nums.length - 1) {
+            for (int i = cur + 1; i <= reachable; i++) { // 1) от следующего (cur+1) до максимального шага (reachable)
+                if (reachable >= nums.length - 1) { // 6) если достигаем последнего, делаем шаг и return
                     ans++;
                     return ans;
                 }
-                if (nums[i] + i > max) {
+                if (nums[i] + i > max) { // 2) ищем максимальный шаг (max - самый опережающий) шаг из возможных
                     max = nums[i] + i;
-                    pos = i;
+                    cur = i; // 3) делаем максимальный - текущим
                 }
             }
-            reachable = nums[pos] + pos;
-            ans++;
+            reachable = nums[cur] + cur; // 4) после нахождения максимального, запоминаем следующие доступные
+            ans++; // 5) делаем шаг
         }
         return ans;
     }
